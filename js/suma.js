eeef586894764726d0645b9bd4f36b3fdb6b00f3
RenzoZeNu <20174091@aloe.ulima.edu.pe>
2023-09-12 16:40:34 -0500

    update-lab05

--- js/suma.js
+++ js/suma.js
@@ -15,18 +15,3 @@ function restar(){
     document.getElementById("rpta").innerHTML = valor_rest
 }
 
-function multiplicar(){
-    let op1 = document.getElementById("id1").value
-    let op2 = document.getElementById("id2").value
-    
-    let valor_mult = parseInt(op1) * parseInt(op2)
-    document.getElementById("rpta").innerHTML = valor_mult
-}
-
-function dividir(){
-    let op1 = document.getElementById("id1").value
-    let op2 = document.getElementById("id2").value
-    
-    let valor_div = parseFloat(op1) / parseFloat(op2)
-    document.getElementById("rpta").innerHTML = valor_div
-}
